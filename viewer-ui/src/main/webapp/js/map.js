9322bf14cdd921b60c2321a6a2767f75832cbbe1
Jorge Lopez Perez <jorgelopez.physics@gmail.com>
2012-05-21 12:16:14 +0200

    Finish merging map.js

--- viewer-ui/src/main/webapp/js/map.js
+++ viewer-ui/src/main/webapp/js/map.js
@@ -8,39 +8,20 @@
 
 var map; 
 var mapPanel;
-<<<<<<< HEAD
-var extent; 
-//var comboGeo;
-
-=======
-var comboGeo;
->>>>>>> 899aec172d513fcb077889f0ef7be72bd7fdfdd5
 
 /**
  *  TreePanel actions
  */
  
-<<<<<<< HEAD
-function initMapPanel() {	
 
-	// if true a google layer is used, if false
-	// the bluemarble WMS layer is used
-	// var google = false;
-	//var google = true; 
-
-=======
 function initMapPanel() {
->>>>>>> 899aec172d513fcb077889f0ef7be72bd7fdfdd5
 	var options = {
 		sphericalMercator: true,
 		projection: new OpenLayers.Projection("EPSG:900913"),
 		units: "m"
 		, maxExtent: new OpenLayers.Bounds(-19567879.238281,-19567879.068281,19567879.238281,19567879.408281)
 		, maxResolution: 156543.0339
-<<<<<<< HEAD
 		, controls: []
-=======
->>>>>>> 899aec172d513fcb077889f0ef7be72bd7fdfdd5
 	};
 	
 	map = new OpenLayers.Map(options);
@@ -48,14 +29,12 @@ function initMapPanel() {
 	// add layers to map 
 	map.addLayers([
 			new OpenLayers.Layer.OSM("OSM Base Map"),
-			/*
 			new OpenLayers.Layer.Google("Google Satellite",
 						{type: google.maps.MapTypeId.SATELLITE 
 						//, numZoomLevels: 12
 						, maxExtent: new OpenLayers.Bounds(-19567879.238281,-19567879.068281,19567879.238281,19567879.408281)
 						}
 			),	
-			
 			new OpenLayers.Layer.Google("Google Streets",
 						{
 						//, numZoomLevels: 12
@@ -68,7 +47,7 @@ function initMapPanel() {
 				}, {
 					isBaseLayer: true
 				}
-			),*/
+			),
 			new OpenLayers.Layer.WMS("Corine 1990 100m",
 				"http://pegasosdi.uab.es/ogc/wms?", {
 					layers: "CORINE_CLC90_100m",
@@ -135,11 +114,8 @@ function initMapPanel() {
 			)
 			
 	]); 
-	
 
 	
-		
-			
 	
 		
 		
@@ -150,13 +126,7 @@ function initMapPanel() {
 		layers: [],
 		center: new OpenLayers.LonLat(18.89,38.58).transform(new OpenLayers.Projection('EPSG:4326'),new OpenLayers.Projection('EPSG:900913')),
 		zoom: 4,
-		//extent: extent,
 		split: true, 
 		plugins:[] // importante para añadir dinámicamente plugins una vez instanciado un panel
 	});
-<<<<<<< HEAD
-	
-	
-=======
->>>>>>> 899aec172d513fcb077889f0ef7be72bd7fdfdd5
 };
