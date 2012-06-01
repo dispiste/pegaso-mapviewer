6ae8a8fe65a5ad3f6f793149b16e343c5711dc79
Jorge Lopez Perez <jorgelopez.physics@gmail.com>
2012-06-01 16:59:44 +0200

    include activeLayers property for the map, compulsory to create legends and visible layer in children web-page for printing

--- viewer-ui/src/main/webapp/js/map.js
+++ viewer-ui/src/main/webapp/js/map.js
@@ -8,6 +8,8 @@
 
 var map; 
 var mapPanel;
+var activeLayers; // activated layers controled from treePanel visibility
+
 
 /**
  *  TreePanel actions
@@ -15,20 +17,21 @@ var mapPanel;
  
 
 function initMapPanel() {
+
 	var options = {
 		sphericalMercator: true,
 		projection: new OpenLayers.Projection("EPSG:900913"),
-		units: "m"
-		, maxExtent: new OpenLayers.Bounds(-19567879.238281,-19567879.068281,19567879.238281,19567879.408281)
-		, maxResolution: 156543.0339
-		, controls: []
+		units: "m", 
+		maxExtent: new OpenLayers.Bounds(-19567879.238281,-19567879.068281,19567879.238281,19567879.408281),
+		maxResolution: 156543.0339, 
+		controls: []
 	};
 	
 	map = new OpenLayers.Map(options);
 
 	// add layers to map 
 	map.addLayers([
-			new OpenLayers.Layer.OSM("OSM Base Map"),
+			//new OpenLayers.Layer.OSM("OSM Base Map"),
 			new OpenLayers.Layer.Google("Google Satellite",
 						{type: google.maps.MapTypeId.SATELLITE 
 						//, numZoomLevels: 12
@@ -127,6 +130,41 @@ function initMapPanel() {
 		center: new OpenLayers.LonLat(18.89,38.58).transform(new OpenLayers.Projection('EPSG:4326'),new OpenLayers.Projection('EPSG:900913')),
 		zoom: 4,
 		split: true, 
-		plugins:[] // importante para añadir dinámicamente plugins una vez instanciado un panel
+		plugins:[] // important in order to add dyncamically  plugins once a panel is instantiated
 	});
+	
+	
 };
+
+
+
+function returnActiveLayersWithClone() {
+						var activ = []; 
+						for (layername in map.layers) {
+									// if the layer isn't visible at this range, or is turned off, skip it
+									var layer = map.layers[layername];
+											if (layer.visibility) {
+													//console.log(layer);
+													var layercl = layer.clone();
+													var layerDiv_cl = layer.div.cloneNode(true);
+													layercl.div = layerDiv_cl;
+													activ.push(layercl); 
+											}
+											
+							}	
+						return activ;
+};
+
+// by executing returnActiveLayersII method, we obtain an array of activated layers called activeLayers; 
+
+function returnActiveLayersII() {
+		// register changelayer event, in which some property of the layers is changed: visibility, name, order, opacity
+		map.events.register('changelayer', null, function(evt){
+					if(evt.property === "visibility") {
+								//alert(evt.layer.name + " layer visibility changed to " + evt.layer.visibility );
+								activeLayers = returnActiveLayersWithClone();
+					}
+		});
+		
+};
+
