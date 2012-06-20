216dc8505a799586dee563cf461272334f56a3a3
Jorge Lopez Perez <jorgelopez.physics@gmail.com>
2012-06-20 16:33:29 +0200

    Add more layers to map and permalink (GeoExt) state Manager

--- viewer-ui/src/main/webapp/js/map.js
+++ viewer-ui/src/main/webapp/js/map.js
@@ -17,18 +17,49 @@ var activeLayers; // activated layers controled from treePanel visibility
  
 
 function initMapPanel() {
-
+	
+	Ext.onReady(function(){
+	
+	// set a permalink provider
+    var permalinkProvider = new GeoExt.state.PermalinkProvider({encodeType: false});
+    Ext.state.Manager.setProvider(permalinkProvider);
+	
+	
+	var windowPermalink = new Ext.Window({
+											title: 'URL Permalink',
+											id: 'ventana',
+											width: 500,
+											autoHeight: true, 
+											border: false,
+											layout: 'fit',
+											closeAction: "hide" // to avoid problems when closing windod!
+											//,items: Ext.getCmp('ventanita_id')
+				}); 
+				
+	//windowPermalink.show();
+	
+	
+	
+	
+	 
+	
 	var options = {
 		sphericalMercator: true,
 		projection: new OpenLayers.Projection("EPSG:900913"),
 		units: "m", 
 		maxExtent: new OpenLayers.Bounds(-19567879.238281,-19567879.068281,19567879.238281,19567879.408281),
 		maxResolution: 156543.0339, 
-		controls: []
+		// controls: []
+		controls: [new OpenLayers.Control.Permalink()]
 	};
 	
 	map = new OpenLayers.Map(options);
 
+	/*
+	var permalink = new OpenLayers.Control.Permalink();
+	map.addControl(permalink);
+	*/
+	
 	// add layers to map 
 	map.addLayers([
 			//new OpenLayers.Layer.OSM("OSM Base Map"),
@@ -105,9 +136,125 @@ function initMapPanel() {
 					visibility: false
 				}
 			),
-			new OpenLayers.Layer.WMS("Country Boundaries",
+			new OpenLayers.Layer.WMS("Natura 2000",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "NATURA2000",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0, 
+					visibility: false
+				}
+			),  
+			new OpenLayers.Layer.WMS("DEM I",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "EDM_USGS_W020N40",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0,
+					visibility: false
+				}
+			), 
+			new OpenLayers.Layer.WMS("DEM II",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "EDM_USGS_E020N40",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0,
+					visibility: false
+				}
+			), 
+			new OpenLayers.Layer.WMS("DEM III",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "EDM_USGS_W020N90",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0,
+					visibility: false
+				}
+			), 
+			new OpenLayers.Layer.WMS("DEM IV",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "EDM_USGS_E020N90",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0,
+					visibility: false
+				}
+			), 
+			new OpenLayers.Layer.WMS("Mediterranean and Black Seas",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "Mediterranean_and_Black_Seas",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0,
+					visibility: false
+				}
+			),/*, 
+			new OpenLayers.Layer.WMS("Country Boundaries 2006",
 				"http://pegasosdi.uab.es/ogc/wms?", {
 					layers: "CNTR_BN_03M_2006",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0
+				}
+			)*/
+			new OpenLayers.Layer.WMS("Country Boundaries 2010",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "CNTR_RG_03M_2010",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0
+				}
+			),
+			new OpenLayers.Layer.WMS("Administrative Units 2010 Level 0",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "NUTS_RG_03M_2010_L0",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0
+				}
+			),
+			new OpenLayers.Layer.WMS("Administrative Units 2010 Level 1",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "NUTS_RG_03M_2010_L1",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0
+				}
+			),
+			new OpenLayers.Layer.WMS("Administrative Units 2010 Level 2",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "NUTS_RG_03M_2010_L2",
+					transparent: true,
+					format: "image/png"
+				}, {
+					isBaseLayer: false,
+					buffer: 0
+				}
+			),
+			new OpenLayers.Layer.WMS("Administrative Units 2010 Level3",
+				"http://pegasosdi.uab.es/ogc/wms?", {
+					layers: "NUTS_RG_03M_2010_L3",
 					transparent: true,
 					format: "image/png"
 				}, {
@@ -117,10 +264,23 @@ function initMapPanel() {
 			)
 			
 	]); 
-
 	
 	
-		
+	
+	
+	var onStatechange = function(provider){
+						if(windowPermalink.rendered == true){
+								link = provider.getLink();
+								Ext.getCmp("ventana").body.update("<a href=" + link + ">" + link + "</a>");
+								//Ext.get("permalinkPanel").doLayout();
+						}
+				};
+				
+	permalinkProvider.on({statechange: onStatechange});
+	
+	
+	
+	
 		
 	mapPanel = new GeoExt.MapPanel({
 		region: "center",
@@ -129,15 +289,29 @@ function initMapPanel() {
 		layers: [],
 		center: new OpenLayers.LonLat(18.89,38.58).transform(new OpenLayers.Projection('EPSG:4326'),new OpenLayers.Projection('EPSG:900913')),
 		zoom: 4,
+		stateId: "map", // to control the state of the Map so as to make permalinks properly
+		prettyStateKeys: true,
 		split: true, 
 		plugins:[] // important in order to add dyncamically  plugins once a panel is instantiated
 	});
 	
+	/*
+	var permalink = new OpenLayers.Control.Permalink();
+	map.addControl(permalink);
+	*/
 	
-};
+	/*
+	// check that there is not a center and alert me!!
+	if (!map.getCenter()){
+		alert("di hola, que me voy");
+	}
+	*/
+	
+ }); // end of Ext.onReady
+ 
+}; // end of initMapPanel() function
 
-
-
+	
 function returnActiveLayersWithClone() {
 						var activ = []; 
 						for (layername in map.layers) {
