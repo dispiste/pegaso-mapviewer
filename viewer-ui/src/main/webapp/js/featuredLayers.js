9fd610633500d2d2af0448387f8e05d825ef78b6
Jorge Lopez Perez <jorgelopez.physics@gmail.com>
2012-07-10 16:02:07 +0200

    file that invokes an instance of Featured Layer ExtJS component

--- viewer-ui/src/main/webapp/js/featuredLayers.js
+++ viewer-ui/src/main/webapp/js/featuredLayers.js
@@ -1,7 +1,3 @@
-
-
-
-
 var botonFL;
 var featuredLayersContainer;
 
@@ -35,25 +31,102 @@ function initFeaturedLayers() {
 						
 									if (btn.pressed){
 										if (!featuredLayersContainer) {
-											var config = {
+											config = {
+											//var config = {
 												categories: [
 												{
 													text: 'Layers 1',
 													imgPath: "img/featuredLayers/imagen_1.png",
 													items: [{
 														text: 'NAME 1',
-														imgPath: "img/featuredLayers/Case_1.png"
+														imgPath: "img/featuredLayers/Case_1.png",
+														server: "http://pegasosdi.uab.es/ogc/wms?", 
+														layer: "CORINE_CLC90_100m"
 													},
 													{
-														text: 'Corine Land Cover',
-														imgPath: "img/featuredLayers/Case_2.png"
+														text: 'NAME 2',
+														imgPath: "img/featuredLayers/Case_2.png",
+														server: "http://pegasosdi.uab.es/ogc/wms?", 
+														layer: "CORINE_CLC00_100m"
+													},
+													{
+														text: 'NAME 3',
+														imgPath: "img/featuredLayers/Case_3.png",
+														server: "http://pegasosdi.uab.es/ogc/wms?", 
+														layer: "CORINE_CLC06_100m"
+													},
+													{
+														text: 'NAME 4',
+														imgPath: "img/featuredLayers/Case_4.png",
+														server: "http://pegasosdi.uab.es/ogc/wms?", 
+														layer: "CORINE_CLC06_250m"
+													}
+													]
+												},
+												{
+													text: 'Indicators I',
+													imgPath: "img/featuredLayers/imagen_2.png",
+													items: [{
+														text: 'NAME 5',
+														imgPath: "img/featuredLayers/FeaturedLayers11_smaller.png",
+														server: "http://www.creaf.uab.es/cgi-bin/MiraMon5_0.cgi?", 
+														layer: "mcsc-catalunya"
+													},
+													{
+														text: 'NAME 6',
+														imgPath: "img/featuredLayers/FeaturedLayers12_smaller.png",
+														server: "http://www.creaf.uab.es/cgi-bin/MiraMon5_0.cgi?", 
+														layer: "mcsc1993-catalunya"
+													},
+													{
+														text: 'NAME 7',
+														imgPath: "img/featuredLayers/FeaturedLayers13_smaller.png",
+														server: "http://www.creaf.uab.es/cgi-bin/MiraMon5_0.cgi?", 
+														layer: "mca-catalunya"
+													},
+													{
+														text: 'NAME 8',
+														imgPath: "img/featuredLayers/FeaturedLayers14_smaller.png",
+														server: "http://www.creaf.uab.es/cgi-bin/MiraMon5_0.cgi?", 
+														layer: "iefc-catalunya"
+													}
+													]
+												},
+												{
+													text: 'Indicators II',
+													imgPath: "img/featuredLayers/imagen_3.png",
+													items: [{
+														text: 'NAME 9',
+														imgPath: "img/featuredLayers/Europe.png", 
+														server: "http://ogc.larioja.org/wms/request.php?",
+														layer: "Acuiferos"
+													},
+													{
+														text: 'NAME 10',
+														imgPath: "img/featuredLayers/Europe.png",
+														server: "http://ogc.larioja.org/wms/request.php?",
+														layer: "Arboles_singulares"
+													},
+													{
+														text: 'NAME 11',
+														imgPath: "img/featuredLayers/Europe.png",
+														server: "http://ogc.larioja.org/wms/request.php?",
+														layer: "Areas_naturales_singulares"
 													}]
 												},
 												{
-													text: 'Indicators',
-													imgPath: "img/featuredLayers/imagen_2.png",
-													items: []
-												}],
+													text: 'Indicators III',
+													imgPath: "img/featuredLayers/imagen_4.png",
+													items: [{
+														text: 'NAME 13',
+														imgPath: "img/featuredLayers/Categorie4_smaller.png"
+													},
+													{
+														text: 'NAME 14',
+														imgPath: "img/featuredLayers/Categorie4_smaller.png"
+													}]
+												}
+												],
 												ownerButton: btn
 											};
 											featuredLayersContainer = new UAB.feat.FeaturedLayers(config);
@@ -65,8 +138,7 @@ function initFeaturedLayers() {
 											easing: 'easeBoth',
 											duration: 0.5
 										});
-									} else {
-											
+									} else {		
 										featuredLayersContainer.getEl().slideOut('t',{
 											easing: 'easeBoth',
 											duration: 0.5
